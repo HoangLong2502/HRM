ae1ff7e231c837f4e38588e8d71f65de336c5650
Hp <long.lbh@idtinc.co>
2022-03-28 18:27:19 +0700

    2

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -13,17 +13,30 @@
             </div>
         </div>
     </div>
-    <div class="bg-white border-radius" style="width:35%">
-        <div>
-            <div>
-
+    <div class="border-radius" style="width:35%">
+        <div class="f boder-radius">
+            <div class="bg-white mr1 border-radius w50">
+                <div class="bg-orange pt1 pb1 pl1 pr1 h5 white" style="border-radius: 8px 8px 0 0;background: linear-gradient(180deg, #FFD422 0%, #F2C800 100%);">
+                    Note
+                </div>
+                <div class="pt1 pr1 pl1 pb2">
+                    <p class="h6">Thẻ thanh toán</p>
+                    <p class="p-small mt05">39 Notes</p>
+                </div>
             </div>
-            <div>
+            <div class="w50 bg-white border-radius pt1 pb1 pr1 pl1">
+                <p class="h6">Thứ hai</p>
+                <p class="h5">22, tháng 5</p>
+                <div>
 
+                </div>
             </div>
         </div>
-        <div>
-
+        <div class="mt2 bg-white border-radius">
+            <div class="bg-orange border-radius pt1 pr1 pl1 pb1">
+                Công việc trong ngày
+            </div>
+            <div></div>
         </div>
     </div>
   </div>
